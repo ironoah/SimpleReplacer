789e7503cda67fb91974b1f9706fdb5ef00952ec
nanashi <nanashi@dummy.com>
2018-08-02 05:47:20 +0900

    first commit

---
+++ SimpleReplacer/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace SimpleReplacer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtBefore = new System.Windows.Forms.TextBox();
+            this.txtAfter = new System.Windows.Forms.TextBox();
+            this.btnReplace = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtBefore
+            // 
+            this.txtBefore.Location = new System.Drawing.Point(12, 12);
+            this.txtBefore.MaxLength = 0;
+            this.txtBefore.Multiline = true;
+            this.txtBefore.Name = "txtBefore";
+            this.txtBefore.Size = new System.Drawing.Size(290, 310);
+            this.txtBefore.TabIndex = 0;
+            this.txtBefore.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtBefore_KeyDown);
+            // 
+            // txtAfter
+            // 
+            this.txtAfter.Location = new System.Drawing.Point(392, 12);
+            this.txtAfter.MaxLength = 0;
+            this.txtAfter.Multiline = true;
+            this.txtAfter.Name = "txtAfter";
+            this.txtAfter.Size = new System.Drawing.Size(290, 310);
+            this.txtAfter.TabIndex = 1;
+            this.txtAfter.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtAfter_KeyDown);
+            // 
+            // btnReplace
+            // 
+            this.btnReplace.Location = new System.Drawing.Point(311, 12);
+            this.btnReplace.Name = "btnReplace";
+            this.btnReplace.Size = new System.Drawing.Size(75, 23);
+            this.btnReplace.TabIndex = 2;
+            this.btnReplace.Text = "Replace";
+            this.btnReplace.UseVisualStyleBackColor = true;
+            this.btnReplace.Click += new System.EventHandler(this.btnReplace_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(693, 334);
+            this.Controls.Add(this.btnReplace);
+            this.Controls.Add(this.txtAfter);
+            this.Controls.Add(this.txtBefore);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtBefore;
+        private System.Windows.Forms.TextBox txtAfter;
+        private System.Windows.Forms.Button btnReplace;
+    }
+}
+
---
+++ SimpleReplacer/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+using System.IO;
+
+namespace SimpleReplacer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnReplace_Click(object sender, EventArgs e)
+        {
+            string dirpath = @"";
+            string filename = "replace_data.tsv";
+            string[] strReplArray = File.ReadAllLines(dirpath + filename, Encoding.UTF8);
+
+            string strBuff = "";
+            strBuff = txtBefore.Text;
+
+            for (int cnt = 0; cnt < strReplArray.GetLength(0); cnt++)
+            {
+                string[] pairStr = strReplArray[cnt].Split('\t');
+
+                StringBuilder strread = new StringBuilder();
+                string[] strarray = strBuff.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
+                for (int i = 0; i < strarray.GetLength(0); i++)
+                {
+                    if (strarray[i].Contains(pairStr[0]) == true)
+                    {
+                        strread.AppendLine(strarray[i].Replace(pairStr[0], pairStr[1]));
+                    }
+                    else
+                    {
+                        strread.AppendLine(strarray[i]);
+                    }
+                }
+                strBuff = strread.ToString();
+            }
+            txtAfter.Text = strBuff;
+        }
+
+        private void txtBefore_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Control && e.KeyCode == Keys.A)
+                txtBefore.SelectAll();
+        }
+        private void txtAfter_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Control && e.KeyCode == Keys.A)
+                txtAfter.SelectAll();
+        }
+    }
+}
